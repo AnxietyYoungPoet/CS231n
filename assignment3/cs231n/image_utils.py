6e229889cebd001ca296db1524fdaa329996b4b2
AnxietyYoungPoet <spq1994@hotmail.com>
2018-08-29 21:48:01 +0800

    fixed a bug in image from url

--- assignment3/cs231n/image_utils.py
+++ assignment3/cs231n/image_utils.py
@@ -60,13 +60,15 @@ def image_from_url(url):
     """
     try:
         f = urllib.request.urlopen(url)
-        _, fname = tempfile.mkstemp()
+        fh, fname = tempfile.mkstemp()
         with open(fname, 'wb') as ff:
             ff.write(f.read())
         img = imread(fname)
+        os.close(fh)
         os.remove(fname)
         return img
     except urllib.error.URLError as e:
+        os.remove(fname)
         print('URL Error: ', e.reason, url)
     except urllib.error.HTTPError as e:
         print('HTTP Error: ', e.code, url)
